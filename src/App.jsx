91fa6e94909d29108579fbc43c0023f8b527e37e
Eliana Zanelli <elianazanelli02@gmail.com>
2023-03-08 20:21:23 -0300

    Examen

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+    <Card />
+  
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>Hola!!</div>
+  )
+}
+
+export default Card
